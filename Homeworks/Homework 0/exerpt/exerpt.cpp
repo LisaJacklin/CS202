fed2df32f371487aafa2a090073a0810bb8123e2
LisaJacklin <89498725+LisaJacklin@users.noreply.github.com>
2022-02-23 22:37:50 -0900

    Add files via upload

---
+++ Homeworks/Homework 0/exerpt/exerpt.cpp
@@ -0,0 +1,20 @@
+//exerpt.cpp 
+//Lisa Jacklin
+//CS 202
+//1/17/2022
+
+//Requirement: write a program that prints an excerpt
+//from a Project Gutenberg book.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    //quote from Charles Dickens, A tale of two cities
+    //then tell wind and fire where to stop," returned madame; "but don't tell me."
+    std::cout << "\"Then tell Wind and Fire where to stop,\" returned madame; \"but don't tell me.\"";
+}
+
