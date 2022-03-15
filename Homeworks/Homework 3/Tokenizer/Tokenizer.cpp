d20789853aaa6eff5d92b8fb0477e4f84817af90
LisaJacklin <lmjacklin@alaska.edu>
2022-03-15 11:07:55 -0800

    tokenizer.cpp requirements/ basic outline set

--- Homeworks/Homework 3/Tokenizer/Tokenizer.cpp
+++ Homeworks/Homework 3/Tokenizer/Tokenizer.cpp
@@ -1,20 +1,41 @@
-// Tokenizer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Tokenizer.cpp 
+// Lisa Jacklin
+// CS 202
+// 3/7/2022
 //
 
+/*
+Requirements:
+- program that takes input from standard into or a file(Command line)
+- prints the tokens along with line and column each token occurs at.
+
+- test program with project gutenberg book 
+- determine how long it takes to process it.
+-diference in time if you skip the output?
+-measure in MB/second
+
+*/
+
 #include <iostream>
+#include <sstream>
+#include <vector>
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+struct TokenAndPosition {
+	string _token;
+	int _line;
+	unsigned int _column;
+};
+vector<string> lineToTokens(const string& line);
+vector<TokenAndPosition> readLines(std::istream& is);
+void printTokens(std::ostream& os, const vector<TokenAndPosition>& tokens);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "start of main" << endl;
+
+	cout << "end of main" << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
