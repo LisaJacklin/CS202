e9bbdfe5e734710b768697ce929ea97db88f978a
LisaJacklin <lmjacklin@alaska.edu>
2022-03-16 19:46:15 -0800

    money.hpp class constructors main

--- Homeworks/Homework 3/money/money.hpp
+++ Homeworks/Homework 3/money/money.hpp
@@ -2,3 +2,41 @@
 //Lisa Jacklin
 //CS 202
 //3/12/2022
+
+#ifndef MONEY_HPP
+#define MONEY_HPP
+
+#include <iostream>
+#include <ostream>
+
+using std::cout;
+using std::endl;
+
+class Money {
+	//boolean operators needed
+
+	//mathematical operators
+
+	//additional operators
+
+private: 
+	int value;
+public:
+	//no money constructor
+	Money() : value(0) {
+
+	}
+
+	//constructor that takes a double
+	Money(double d) {
+
+	}
+	//constructor that takes dollars and cents
+	Money(int d, int c) {
+
+	}
+
+};
+
+
+#endif
