48f03f4e9610df78c8fbc649ab6673deb53402fd
LisaJacklin <lmjacklin@alaska.edu>
2022-03-26 15:10:44 -0800

    columns.cpp created

---
+++ Practice/Midterm/Bookread/bookread.hpp
@@ -0,0 +1,26 @@
+#ifndef BOOKREAD_HPP
+#define BOOKREAD_HPP
+
+#include <iostream>
+#include <ostream>
+#include <istream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using std::istream;
+using std::ostream;
+
+using std::getline;
+using std::istringstream;
+using std::cout;
+using std::endl;
+
+//taking tokensandpositions and making it a class
+class TokensAndPositions {
+
+};
+
+#endif
---
+++ Practice/Midterm/columns/columns.cpp
@@ -0,0 +1,13 @@
+// columns.cpp
+// Lisa Jacklin
+// CS 202 Midterm Part 2
+// 3/24/ 2022
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
