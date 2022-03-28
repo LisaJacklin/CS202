21c4d6b08a1d9df9a8e7095b52a643def5c97329
LisaJacklin <lmjacklin@alaska.edu>
2022-03-28 12:22:09 -0800

    columns.cpp/hpp code from bookread.cpp/hpp added as base

--- Practice/Midterm/columns/columns.cpp
+++ Practice/Midterm/columns/columns.cpp
@@ -4,10 +4,52 @@
 // 3/24/ 2022
 //
 
+
 #include <iostream>
+#include "bookread.hpp"
+#include <fstream>
+#include <iostream>
+using std::cout;
+using std::endl;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using std::ifstream;
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char** argv) {
+    //remember this has to be true to read!
+    bool fileMode = true;
+    //file name to read
+    string fileName = "980.txt";
+    bool printing = true;
+
+    for (int i = 1; i < argc; i++) {
+
+        if (string(argv[i]) == "--lineonly") {
+            printing = false;
+        }
+        else {
+            //must be file name
+            fileName = argv[i];
+            fileMode = true;
+        }
+
+    }
+    //error tester
+    if (!fileMode) {
+        cout << "Expected a file as parameter" << endl;
+        return 0;
+    }
+
+    vector<Token> tokens;
+    ifstream is(fileName);
+
+    tokens = readLines(is);
+
+    if (printing) {
+        printTokens(cout, tokens);
+
+    };
+
 }
-
---
+++ Practice/Midterm/columns/columns.hpp
@@ -0,0 +1,97 @@
+#ifndef BOOKREAD_HPP
+#define BOOKREAD_HPP
+
+#include <iostream>
+#include <ostream>
+#include <istream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using std::ostream;
+using std::istream;
+using std::string;
+using std::find;
+using std::vector;
+using std::istringstream;
+using std::ostringstream;
+using std::getline;
+
+struct Token {
+    string token;
+    int paragraphs;
+};
+
+vector<string> lineToTokens(const string& line);
+vector<Token> readLines(istream& is);
+void printTokens(ostringstream& os, vector<Token>& tokens);
+
+vector<Token> readLines(istream& is) {
+    vector<Token> toRet;
+    int Num = 0;
+    string line;
+
+    while (true) {
+        getline(is, line);
+        if (!is) {
+            if (is.eof()) {
+                //file is complete
+                break;
+            }
+            //error....
+            return toRet;
+        }
+        //process
+        Num++;
+        vector<string> tokens = lineToTokens(line);
+        for (string s : tokens) {
+            Token t;
+            t.token = s;
+            t.paragraphs = Num;
+            toRet.push_back(t);
+        }
+    }
+    return toRet;
+
+}
+
+vector<string> lineToTokens(const string& line) {
+    vector<string> toRet;
+    istringstream is(line);
+    string token;
+
+    while (true) {
+        is >> token;
+        if (!is) {
+            if (is.eof()) {
+                break;
+            }
+            //Broken, but not much we can do
+            return toRet;
+        }
+        toRet.push_back(token);
+    }
+    return toRet;
+}
+
+
+void printTokens(ostream& os, vector<Token>& tokens) {
+    // vector<Token> tokens;
+    for (Token t : tokens) {
+        //okay now, a wrapping function to make the words fit the correct requirements.
+        int characters = 50;
+        if (t.token.length() < characters) {
+            os << t.token << " ";
+            size_t space_left = characters - t.token.length();
+            if (space_left < t.token.length() + 1) {
+                os << " " << t.token;
+                space_left -= t.token.length() + 1;
+            }
+        }
+        else {
+            os << "\n" << t.token;
+            int characters = 50;
+        }
+    }
+}
+#endif
