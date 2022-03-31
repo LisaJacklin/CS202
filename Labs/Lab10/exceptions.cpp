fcf1499f84500f2e4604f57c85a84835f5d9746c
LisaJacklin <lmjacklin@alaska.edu>
2022-03-31 14:05:53 -0800

    exceptions.cpp error with adding class member

--- Labs/Lab10/exceptions.cpp
+++ Labs/Lab10/exceptions.cpp
@@ -10,34 +10,49 @@ using std::endl;
 
 /*
 Requirements:
-1. functionc contains exactly one statement
+1. functionc contains exactly one statement (x)
 2. functionb declares an object of class ( construct and destruct not)
 3. output!
-- starting main
+- starting main (X)
 - object constructed message
-- starting function b
+- starting function b (X)
 - object destruction message
-- cought an exception: function c threw
+- cought an exception: function c threw (included but not operating)
 - ended normally
 
 */
 
+class labs {
+    labs() {
+        cout << "labs constructor" << endl;
+    }
+    ~labs() {
+        cout << "labs deconstructor" << endl;
+    }
+};
+
 void functionC(){
     //your code here
+    std::runtime_error("Function C threw std::runtime_error");
 }
 
 void functionB(){
- // your code here
+    labs L; // not taking my class parameter...
     cout << "Starting functionB()" << endl;
     functionC();
     cout << "Ending functionB()" << endl;
 }
 
 void functionA(){
-    functionB();
-    // your code here
-    cout << "Caught an exception: " << e.what() << endl;
-    // your code here
+    try {
+        //function B moved into my try 
+        functionB();
+    }
+    catch (const std::exception &e) {
+        cout << "Caught an exception: " << e.what() << endl;
+    //since this line is to tell if it's caught, I placed it
+        //within my catch with e being the exception.
+    }
 }
 int main() {
     cout << "Starting main()" << endl;
