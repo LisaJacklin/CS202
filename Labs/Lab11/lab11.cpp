6e2362a6a247d64253531a94d9f8a7f08a82f26a
LisaJacklin <lmjacklin@alaska.edu>
2022-04-07 10:15:41 -0800

    lab11.cpp start to derived/base /main complete

--- Labs/Lab11/lab11.cpp
+++ Labs/Lab11/lab11.cpp
@@ -7,22 +7,47 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::string;
 
 class Base {
 public:
-	Base() {
+	Base() { //base class constructor
 		cout << "Constructing a default Base object" << endl;
 	}
-	Base(int _var) {
+
+	Base(int i) : _var(i) { //base class variable constructor
+		cout << "Base constructor with _var: " << _var << endl;
+	}
+
+	~Base() { //deconstructor of base class
 		_var = 1;
-		cout << "variable number" << _var << endl;
+		cout << < "deconstructor called _var number: " << _var << endl;
 	}
 private:
 	int _var;
 };
 
+class second : public Base {
+public:
+	second(string s, int i) : Base(i), _name(s) {
+		cout << "Constructor with a name: " << _name << endl;
+	}
+	~second() {
+		cout << "destructor of : " << _name << endl;
+	}
+private:
+	string _name;
+};
+
 int main() {
 	cout << "start of main" << endl;
+
+	Base a;
+	Base b(3);
+
+	//second c;
+	second c("Jacob", 3);
+
 
 	cout << "end of main" << endl;
 }
