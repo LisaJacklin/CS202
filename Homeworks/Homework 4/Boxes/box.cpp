960512c547a48d47e8d23721ccbdc58e54055ccc
LisaJacklin <lmjacklin@alaska.edu>
2022-04-07 21:27:10 -0800

    box.cpp outline begun

---
+++ Homeworks/Homework 4/Boxes/box.cpp
@@ -0,0 +1,15 @@
+//Box.cpp
+// Lisa Jacklin
+// CS 202 Homework 4
+// 4/5/2022
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "start of main()" << endl;
+
+	cout << "end of main()" << endl;
+}
