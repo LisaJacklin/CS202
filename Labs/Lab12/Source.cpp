e252ca4f7798cc619758ee35a934c7fb904d3d76
LisaJacklin <lmjacklin@alaska.edu>
2022-04-09 14:08:51 -0800

    lab12 source.cpp begun

---
+++ Labs/Lab12/Source.cpp
@@ -0,0 +1,15 @@
+//Source.cpp
+// Lisa Jacklin
+// CS 202 Lab 12
+// 4/8/2022
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "start of main()" << endl;
+
+	cout << "end of main()" << endl;
+}
