f21b1aeef40d898adab903e6bfadadd35743eb2a
LisaJacklin <lmjacklin@alaska.edu>
2022-04-10 12:05:43 -0800

    box.cpp base class and derived added

--- Homeworks/Homework 4/Boxes/box.cpp
+++ Homeworks/Homework 4/Boxes/box.cpp
@@ -4,9 +4,43 @@
 // 4/5/2022
 //
 
+/*
+* Requirements:
+* - write a set of classes representing boxes (which can be displayed)
+* - include width and height and can be printed to screen or file
+* - boxes can be hollow, filled or checkered...
+* 
+* Abstract base called Box,
+* Derived: filledBox, HollowBox, and CheckeredBox
+* 
+* 
+*/
+
 #include <iostream>
 using std::cout;
 using std::endl;
+
+//Abstract Base class
+class Box {
+public:
+private:
+};
+
+class FilledBox : public Box {
+public:
+private:
+
+};
+
+class HollowBox : public Box {
+public:
+private:
+};
+
+class CheckeredBox : public Box {
+public:
+private:
+};
 
 int main() {
 	cout << "start of main()" << endl;
