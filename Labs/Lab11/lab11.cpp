1dfac82fa5696f6e5c11d1a89a6ae6f91e4b2cbf
LisaJacklin <lmjacklin@alaska.edu>
2022-04-11 15:36:49 -0800

    lab11.cpp complete and output is correct

--- Labs/Lab11/lab11.cpp
+++ Labs/Lab11/lab11.cpp
@@ -12,42 +12,46 @@ using std::string;
 class Base {
 public:
 	Base() { //base class constructor
-		cout << "Constructing a default Base object" << endl;
-	}
+		cout << "Constructing a default Base object" << endl;}
 
 	Base(int i) : _var(i) { //base class variable constructor
-		cout << "Base constructor with _var: " << _var << endl;
-	}
+		cout << "Base constructor with _var: " << _var << endl;}
 
 	~Base() { //deconstructor of base class
-		_var = 1;
-		cout << < "deconstructor called _var number: " << _var << endl;
-	}
+		cout << "deconstructor called _var number: " << _var << endl;}
 private:
 	int _var;
 };
 
-class second : public Base {
+class Derived : public Base {
 public:
-	second(string s, int i) : Base(i), _name(s) {
-		cout << "Constructor with a name: " << _name << endl;
-	}
-	~second() {
-		cout << "destructor of : " << _name << endl;
-	}
+	Derived(): _name("")  { //if no name is given...though might workout without? test that
+		cout << "Constructing a default Derived object." << endl;}
+
+	Derived(string s, int i) : Base(i), _name(s) {
+		cout << "Constructor with a name: " << _name << endl;}
+
+	~Derived() {
+		cout << "destructor of object named:  " << _name << endl;}
+
 private:
 	string _name;
 };
 
 int main() {
-	cout << "start of main" << endl;
+	//cout << "start of main" << endl;
 
-	Base a;
-	Base b(3);
+	//the code below is given from lab 11 guidelines
+	{Base b; }
+	cout << endl;
+	{Base b(13); }
+	cout << endl;
 
-	//second c;
-	second c("Jacob", 3);
+	{Derived d; }
+	cout << endl;
+
+	{Derived d("Eliza", 16); }
 
 
-	cout << "end of main" << endl;
+	//cout << "end of main" << endl;
 }
