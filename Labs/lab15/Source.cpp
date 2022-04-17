30a130d51958018582fa840abcfdc483eb7888a2
LisaJacklin <lmjacklin@alaska.edu>
2022-04-17 11:18:20 -0800

    Wrapper.hpp added and begun

---
+++ Labs/lab15/Source.cpp
@@ -0,0 +1,36 @@
+//Source.cpp
+// Lisa Jacklin
+// CS 202 Lab 15
+// 4/17/2022
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+/*
+Requirements:
+- write a class template "wrapper" containing an object of any type
+- need constructor that takes object w type.
+- should contain an overloaded operator <<
+	- will insert the wrapped object into the ostream. ( pr a public print() )
+	-this operator doesn't need to be a friend.
+
+- place the class in a Wrapper.hpp file
+- do not define your member class function in the class def.
+- demonstrate:
+Wrapper<int> w{2};
+Wrapper <string> s {"Hellow world!"};
+cout <<  w << " " << s << endl;
+
+*/
+
+int main() {
+	cout << "start of main" << endl;
+
+	Wrapper<int> w{ 2 };
+	Wrapper <string> s{ "Hellow world!" };
+	cout << w << " " << s << endl;
+
+	cout << "end of main" << endl;
+}
---
+++ Labs/lab15/Wrapper.hpp
@@ -0,0 +1,10 @@
+#endif WRAPPER_HPP
+#define WRAPPER_HPP
+
+#include <iostream>
+using std::ostream;
+
+
+
+
+#endif
