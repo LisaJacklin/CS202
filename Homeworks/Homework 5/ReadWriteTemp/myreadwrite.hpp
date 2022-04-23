9fa8e109b5f5235225af9c24fc385a4c6a47a59a
LisaJacklin <lmjacklin@alaska.edu>
2022-04-23 09:41:33 -0800

    myreadwrite functions write and read created and passed

--- Homeworks/Homework 5/ReadWriteTemp/myreadwrite.hpp
+++ Homeworks/Homework 5/ReadWriteTemp/myreadwrite.hpp
@@ -1,11 +1,28 @@
 #ifndef MYREADWRITE_HPP
 #define MYREADWRITE_HPP
 
+//myreadwrite.hpp
+// Lisa Jacklin
+// CS 202 Homework 5
+// 4/23/2022
+//
+
 #include <iostream>
 using std::cout;
 using std::cin;
 
+template <typename T>
+void myWrite(std::ofstream& w, const T& x) {
+	//note that we are writing and reading to a file
+	//thus we can use const char *
+	w.write(reinterpret_cast<const char*>(&x), sizeof(T));
 
+}
 
+template <typename T>
+void myRead(std::ifstream& r, T& x) {
+	//cannot use const char otherwise the same var will print
+	r.read(reinterpret_cast<char*> (&x), sizeof(T));
+}
 
 #endif
