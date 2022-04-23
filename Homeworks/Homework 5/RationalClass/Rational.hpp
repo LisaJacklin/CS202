c0549b0d1d628b4bf587333f41cca3ad7aa2a1a5
LisaJacklin <lmjacklin@alaska.edu>
2022-04-23 10:23:49 -0800

    rational.hpp template class and overiding operators opened to be adjusted.

--- Homeworks/Homework 5/RationalClass/Rational.hpp
+++ Homeworks/Homework 5/RationalClass/Rational.hpp
@@ -2,9 +2,16 @@
 #define RATIONAL_HPP
 #include <iostream>
 
+//Rational.hpp
+//CS 202 Sample Code
+//Modified by Lisa Jacklin
+
 // 3/2, 5/1, 10/2, 1008882/23428, 1/2*1/3==1/6
 // class invariant:
 // _denominator is always > 0
+
+//now a template class
+template <typename T> 
 class Rational {
 
 private:
@@ -15,12 +22,11 @@ private:
 public:
    // Rational() { _numerator=0; _denominator=1; }
    Rational(int,int=1);
+
    friend std::ostream& operator<<(std::ostream &, const Rational &);
+   //does this one need to be a friend? 
    Rational & operator+=(const Rational &);
    friend Rational operator+(const Rational &, const Rational &);
-
-
-#if 0
    friend Rational operator-(const Rational &lhs);
    friend bool operator==(const Rational &lhs, const Rational &rhs);
    friend bool operator<(const Rational &lhs, const Rational &rhs);
@@ -33,19 +39,21 @@ public:
    Rational operator++(int); //postfix ++
    Rational & operator--();        //prefix --
    Rational operator--(int); //postfix --
-private:
 
-#endif
+
 };
 
-#if 0
+//these can be used as a base for the template versions
+//rational opperators ...
 Rational operator-(const Rational &lhs, const Rational &rhs);
 Rational operator*(Rational lhs, const Rational &rhs);
 Rational operator/(Rational lhs, const Rational &rhs);
+
+//boolean operators
 bool operator!=(const Rational &lhs, const Rational &rhs);
 bool operator>(const Rational &lhs, const Rational &rhs);
 bool operator<=(const Rational &lhs, const Rational &rhs);
 bool operator>=(const Rational &lhs, const Rational &rhs);
-#endif
+
 
 #endif
