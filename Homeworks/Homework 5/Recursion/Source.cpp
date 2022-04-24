22c3ea0d7100b285231f6d43b411d93b04e4350d
LisaJacklin <lmjacklin@alaska.edu>
2022-04-23 18:14:58 -0800

    source.cpp outline functions for fib/fib_loop/ack

---
+++ Homeworks/Homework 5/Recursion/Source.cpp
@@ -0,0 +1,29 @@
+//source.cpp
+// Lisa Jacklin
+// CS 202 Homework 5 Recursion
+// 4/20/2022
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int fib_loop(int n) {
+
+}
+
+
+int fib(int n) {
+
+}
+
+int ack(int n) {
+
+}
+
+
+int main() {
+	cout << "start of main" << endl;
+
+	cout << "end of main" << endl;
+}
