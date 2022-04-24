106e07afc66d0fe97a6ac06f502b88159f716700
LisaJacklin <lmjacklin@alaska.edu>
2022-04-23 18:21:42 -0800

    source.cpp fib function compiles

--- Homeworks/Homework 5/Recursion/Source.cpp
+++ Homeworks/Homework 5/Recursion/Source.cpp
@@ -8,22 +8,37 @@
 using std::cout;
 using std::endl;
 
-int fib_loop(int n) {
-
-}
-
-
+//note the use of integer functions because everything delt w/ is a number
 int fib(int n) {
+	//returns the nth number of the sequence
+	if (n == 0) return 0; // nothing there, nothing to give
+	if (n == 1) return 1; //first number in the sequence is 1
+	//note the equation here comes from the equation given 
+	//in homework guide Fn = Fn-1 + Fn-2
+	return fib(n - 1) + fib(n - 2);
 
 }
 
-int ack(int n) {
+int fib_loop(int n) {
+	//returns the nth number in the sequence without recursion...
+	return 0;
+}
 
+int ack(int m, int n) {
+	//Ackermann's number 
+
+	return 0;
 }
 
 
 int main() {
 	cout << "start of main" << endl;
+
+	cout << "Fibonacci Sequence" << endl;
+	for (int i = 0; i < 20; i++) {
+		cout << "fib(" << i << "): " << fib(i) << endl;
+	}
+
 
 	cout << "end of main" << endl;
 }
