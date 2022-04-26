cdec7586f06ab1c4ad85a5ff06ab898dd795c44e
LisaJacklin <lmjacklin@alaska.edu>
2022-04-25 19:06:27 -0800

    source.cpp outline began

---
+++ Practice/FINAL/multicolumns/multicolumns/Source.cpp
@@ -0,0 +1,15 @@
+//Source.cpp
+// Lisa Jacklin
+// CS 202 FINAL
+// 4/25/2022
+//
+
+#include <iostream>
+using std::endl;
+using std::cout;
+
+int main() {
+	cout << "start of main" << endl;
+
+	cout << "end of main" << endl;
+}
